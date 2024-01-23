a442d3ea709599f4c04b9550d07d4b5f34c4c9b1
Eric Lund <eric.lund@hpe.com>
2024-01-23 11:24:15 -0600

    Enable github actions for validation

--- runPrep.sh
+++ runPrep.sh
@@ -1,18 +1,19 @@
+#!/bin/bash
 #
 # MIT License
-#
+# 
 # (C) Copyright 2024 Hewlett Packard Enterprise Development LP
-#
+# 
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
 # to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense,
 # and/or sell copies of the Software, and to permit persons to whom the
 # Software is furnished to do so, subject to the following conditions:
-#
+# 
 # The above copyright notice and this permission notice shall be included
 # in all copies or substantial portions of the Software.
-#
+# 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -20,30 +21,13 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-#
-name: Python (lint)
 
-on: [push]
-
-jobs:
-  lint:
-    name: Lint
-    strategy:
-      matrix:
-        version: ['3.11', '3.10', '3.9', '3.8']
-    runs-on: ubuntu-latest
-    steps:
-    - uses: actions/checkout@v4
-      with:
-        fetch-depth: 0 # Also fetch git-tags
-    - name: Set up Python
-      uses: actions/setup-python@v5
-      with:
-        python-version: ${{ matrix.version }}
-    - name: nox
-      env:
-        GIT_REPO_NAME: "${GITHUB_REPOSITORY#*/}"
-      run: |
-        pip install --extra-index-url=https://artifactory.algol60.net/artifactory/csm-python-modules/simple .[ci]
-        pip install --extra-index-url=https://artifactory.algol60.net/artifactory/csm-python-modules/simple .[lint]
-        nox -e lint
+# Set up .netrc so we can install from algol60.net
+ALGOL60_HOST="artifactory.algol60.net"
+> "${HOME}/.netrc"
+chmod 600 "${HOME}/.netrc"
+cat <<EOF >> "${HOME}/.netrc"
+machine ${ALGOL60_HOST}
+    login ${ARTIFACTORY_USERNAME}
+    password ${ARTIFACTORY_PASSWORD}
+EOF
